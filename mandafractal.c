692769f1d913a7060fa7ca15f6df23b93f1c7938
unknown <computergeek3.142@gmail.com>
2019-06-15 14:16:49 -0700

    mandafractal.c example can render julia sets when user presses ' '

--- mandafractal.c
+++ mandafractal.c
@@ -7,12 +7,27 @@
 
 unsigned char color_scheme[7] = {COLOR_RED, COLOR_YELLOW, COLOR_WHITE, COLOR_GREEN, COLOR_MAGENTA, COLOR_CYAN, COLOR_BLUE};
 
+complex double global_c = 0;
+
 unsigned int mandelbrot_iterations(complex double c, unsigned int max_iterations){
 	complex double z = 0;
 	unsigned int i;
 
 	for(i = 0; i < max_iterations; i++){
 		z = z*z + c;
+		if(cabs(z) > 2){
+			return i;
+		}
+	}
+
+	return max_iterations;
+}
+
+unsigned int julia_iterations(complex double z, unsigned int max_iterations){
+	unsigned int i;
+
+	for(i = 0; i < max_iterations; i++){
+		z = z*z + global_c;
 		if(cabs(z) > 2){
 			return i;
 		}
@@ -52,6 +67,12 @@ int main(){
 	unsigned int num_iterations = 50;
 	double width = 4;
 	complex double center = 0;
+	double old_width = 4;
+	complex double old_center = 0;
+	unsigned int old_num_iterations = 50;
+	unsigned char mode = 1;
+	unsigned int (*iterations_func)(complex double, unsigned int) = mandelbrot_iterations;
+	unsigned char do_render;
 
 	initscr();
 	start_color();
@@ -60,37 +81,64 @@ int main(){
 
 	s = CAM_screen_create(stdscr, COLS - 1, LINES);
 	while(key_hit != 'q'){
-		render(s, mandelbrot_iterations, center, width, num_iterations);
+		render(s, iterations_func, center, width, num_iterations);
 		CAM_update(s);
 		refresh();
-		key_hit = getch();
-		switch(key_hit){
-			case KEY_UP:
-				center += I*width/4;
-				break;
-			case KEY_DOWN:
-				center -= I*width/4;
-				break;
-			case KEY_LEFT:
-				center -= width/4;
-				break;
-			case KEY_RIGHT:
-				center += width/4;
-				break;
-			case '+':
-				width /= 4;
-				break;
-			case '-':
-				width *= 4;
-				break;
-			case '>':
-				num_iterations *= 2;
-				break;
-			case '<':
-				num_iterations /= 2;
-		}
+		do{
+			key_hit = getch();
+			do_render = 1;
+			switch(key_hit){
+				case KEY_UP:
+					center += I*width/4;
+					break;
+				case KEY_DOWN:
+					center -= I*width/4;
+					break;
+				case KEY_LEFT:
+					center -= width/4;
+					break;
+				case KEY_RIGHT:
+					center += width/4;
+					break;
+				case '+':
+					width /= 4;
+					break;
+				case '-':
+					width *= 4;
+					break;
+				case '>':
+					num_iterations *= 2;
+					break;
+				case '<':
+					num_iterations /= 2;
+					break;
+				case ' ':
+					mode = !mode;
+					if(mode){
+						width = old_width;
+						center = old_center;
+						num_iterations = old_num_iterations;
+						iterations_func = mandelbrot_iterations;
+					} else {
+						old_width = width;
+						old_center = center;
+						old_num_iterations = num_iterations;
+						global_c = center;
+						width = 4;
+						center = 0;
+						iterations_func = julia_iterations;
+					}
+					break;
+				case 'q':
+					break;
+				default:
+					do_render = 0;
+			}
+		} while(!do_render);
 	}
 	CAM_screen_free(s);
 	endwin();
+
+	return 0;
 }
 
